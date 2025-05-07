683074275d208fbbac520c79a94e54049fb3476e
Bruno Fontana <obrunofontana@gmail.com>
2025-05-07 09:18:14 -0300

    fix: remove importacoes n utilizadas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Playfair_Display, Raleway } from "next/font/google";
+import { Playfair_Display, Raleway } from "next/font/google";
 import "./globals.css";
 
 const raleway = Raleway({
